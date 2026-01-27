4d45d3b9d36bc64ec660bbe58ab367362fd04e29
Daniel Yepez Garces <git@dyepezg.dev>
2026-01-27 18:51:16 -0500

    Create equipos.php to list Colombian football teams
    Added a PHP file listing Colombian football teams with links.

---
+++ colombia/masculina/equipos.php
@@ -0,0 +1,30 @@
+<?php
+return [
+
+    "Alianza" => "[[Alianza Fútbol Club (Colombia)|Alianza Valledupar]]",
+    "América de Cali" => "[[América de Cali]]",
+    "Atlético Bucaramanga" => "[[Atlético Bucaramanga]]",
+    "Junior" => "[[Junior de Barranquilla|Junior]]",
+    "Atlético Nacional" => "[[Atlético Nacional]]",
+    "Boyaca Chico" => "[[Boyacá Chicó]]",
+    "Deportes Tolima" => "[[Deportes Tolima]]",
+    "Deportivo Cali" => "[[Deportivo Cali]]",
+    "Deportivo Pasto" => "[[Deportivo Pasto]]",
+    "Deportivo Pereira" => "[[Deportivo Pereira]]",
+    "Envigado" => "[[Envigado Fútbol Club|Envigado F. C.]]",
+    "Fortaleza" => "[[Fortaleza Fútbol Club|Fortaleza]]",
+    "Independiente Medellín" => "[[Deportivo Independiente Medellín|Independiente Medellín]]",
+    "Internacional de Bogota" => "[[Internacional de Bogotá]]",
+    "Llaneros" => "[[Club Llaneros|Llaneros]]",
+    "Millonarios" => "[[Millonarios Fútbol Club|Millonarios]]",
+    "Once Caldas" => "[[Once Caldas]]",
+    "Independiente Santa Fe" => "[[Club Independiente Santa Fe|Santa Fe]]",
+    "Unión Magdalena" => "[[Unión Magdalena]]",
+    "Águilas Doradas" => "[[Águilas Doradas]]",
+    "Cúcuta Deportivo" => "[[Cúcuta Deportivo]]",
+    "Jaguares de Córdoba" => "[[Jaguares de Córdoba|Jaguares]]",
+
+    // Variantes
+    "Boyacá Chicó" => "[[Boyacá Chicó]]",
+    "Aguilas Doradas" => "[[Águilas Doradas]]",
+];
