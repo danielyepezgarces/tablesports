5d59c4f5a8befb096f962d9a5479da0977f5c6f4
Daniel Yepez Garces <git@dyepezg.dev>
2026-03-18 22:45:21 -0500

    Update admin password hash in config.php

--- admin/config.php
+++ admin/config.php
@@ -21,7 +21,7 @@ if (file_exists($localConfig)) {
     // -----------------------------------------------------------------------
 
     /** Bcrypt hash of the admin password. */
-    define('ADMIN_PASSWORD_HASH', '$2y$10$5dBLdLS/pJErg/dZerEgCOWL9F3us72NPYgRZgFj94II0XgTo6stK');
+    define('ADMIN_PASSWORD_HASH', '$2a$12$h7eNLfXsUwvQKM2oXD4p4evfjlJ47LcCbPbZuFg26JR5iiUy0e3wi');
 
     /** Absolute path to the SQLite3 database file. */
     define('DB_PATH', __DIR__ . '/../data/jugadores.db');
